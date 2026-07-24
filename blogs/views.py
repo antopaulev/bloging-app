301902ade757c407f6f288dc2aa061ab2d11aba0
antopaulev <antopaul2022@gmail.com>
2026-07-24 13:57:19 +0530

    made changes in home page and started working on category page

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render, get_object_or_404 
+from . models import Blog, Category
 
 # Create your views here.
+def posts_by_category(request, category_id):
+    posts = Blog.objects.filter(status = 'Published' , category = category_id)
+    #use try/catch if i want to use custom method for handling error and redirect it to home page
+    try:
+        category = Category.objects.get(pk = category_id)
+    except:
+        return redirect('home')
+    # use this if i want to show 404 default page if the object is not found
+    # category = get_object_or_404(Category, pk = category_id)
+    context = {
+        'posts' : posts,
+        'category': category,
+    }
+    return render(request, "post_by_category.html", context)
