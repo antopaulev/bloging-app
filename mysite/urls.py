f5f230145c28ac376ce01c0d060b7cb834573b92
antopaulev <antopaul2022@gmail.com>
2026-07-27 20:11:32 +0530

    created web page to dispalyblogs

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,9 +20,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from blogs import views as Blogviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home , name = 'home'),
     path('category/',include('blogs.urls')),
+    path('<slug:slug>/', Blogviews.blogs , name = 'blogs'),
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
