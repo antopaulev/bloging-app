6c2ad598ae617fc40ea560c1187ea7a1e6c12e72
antopaulev <antopaul2022@gmail.com>
2026-08-12 18:33:47 +0530

    working with the dashboard

---
+++ dashboards/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import redirect, render
+from blogs.models import Category
+from django.contrib.auth.decorators import login_required
+from .forms import CategoryForm
+
+# Create your views here.
+@login_required(login_url='login')
+def dashboard(request):
+    category_count = Category.objects.all().count()
+    blogs_count = Category.objects.all().count()
+    context = {
+        'category_count': category_count,
+        'blogs_count': blogs_count,
+    }
+    return render(request, 'dashboards/dashboard.html', context)
+
+def categories(request):
+    return render(request, 'dashboards/categories.html')
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    
+    form = CategoryForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'dashboards/add_category.html', context)
