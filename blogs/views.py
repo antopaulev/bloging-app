1ac004cad90b6d9e1cde26515785db5278baf8ca
antopaulev <antopaul2022@gmail.com>
2026-08-17 21:12:16 +0530

    adding comment features is completed

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render, get_object_or_404 
-from . models import Blog, Category
+from . models import Blog, Category,Comment
 from django.db.models import Q
 
 # Create your views here.
@@ -20,9 +20,21 @@ def posts_by_category(request, category_id):
     return render(request, "post_by_category.html", context)
 
 def blogs(request, slug):
-    single_blog = get_object_or_404(Blog, slug = slug) 
+    single_blog = get_object_or_404(Blog, slug = slug)
+    if request.method == 'POST' :
+        comment = Comment()
+        comment.user = request.user
+        comment.blog = single_blog
+        comment.comment = request.POST['comment']
+        comment.save()
+        return HttpResponseRedirect(request.path_info)
+    #comment
+    comments = Comment.objects.filter(blog=single_blog)
+    comment_count = comments.count
     context = {
         'single_blog' : single_blog,
+        'comments':comments,
+        'comment_count' : comment_count,
     }
     return render(request, 'blogs.html', context )
 
