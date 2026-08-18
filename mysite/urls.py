49830e4fd8611c76d4c27351a3f7ef632cfa9a52
antopaulev <antopaul2022@gmail.com>
2026-08-18 20:05:24 +0530

    final commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('', views.home , name = 'home'),
     path('category/',include('blogs.urls')),
     path('blogs/<slug:slug>/', Blogviews.blogs , name = 'blogs'),
-    path('blogs/search/', Blogviews.search, name = 'search'),
+    path('search/', Blogviews.search, name = 'search'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name = 'login'),
     path('logout/', views.logout, name = 'logout'),
